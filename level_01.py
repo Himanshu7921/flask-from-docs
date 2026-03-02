30166f573369dee12658395c86121165325e7651
Himanshu7921 <himanshusr451tehs@gmail.com>
2026-03-02 15:49:17 +0530

    Added level-01, task-03

--- level_01.py
+++ level_01.py
@@ -36,6 +36,39 @@ def square_num(num: int):
     return f"{num ** 2}"
 
 
+# Task=03: Query Parameters
+@app.route("/query")
+def get_name():
+    name = request.args.get("name")
+    age = request.args.get("age")
+    occupation = request.args.get("occupation")
+    if name is not None and age is not None and occupation is not None:
+        return jsonify({
+            "name": name,
+            "age": age,
+            "occupation": occupation
+        })
+    else:
+        return "Pass Query as name, age and occupation in URL"
+
+@app.route("/calc")
+def calculator():
+    num = int(request.args.get("num"))
+    op = request.args.get("op")
+    
+    if num is None:
+        return "Please pass number as Query as ('?num=10')"
+    
+    if op is None:
+        return "Please pass op as Query as ('?op=operator'), available operators = ['cube', 'square', 'sqrt']"
+    
+    if op == "sqrt":
+        return f"sqrt({num}) = {math.sqrt(num)}"
+    elif op == "square":
+        return f"square({num}) = {math.pow(num, 2)}"
+    elif op == "cube":
+        return f"cube({num}) = {math.pow(num, 3)}"
+
 if __name__ == "__main__":
     app.run(debug=True)
 
